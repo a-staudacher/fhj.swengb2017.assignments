84383dbc44d2ef5d12167d2481720718093bf783
Andreas Staudacher <andreas.staudacher@outlook.de>
2017-12-21 18:33:04 +0100

    Protobuf messages and started converters

--- battleship/model/src/main/scala/at/fhj/swengb/apps/battleship/BattleShipProtocol.scala
+++ battleship/model/src/main/scala/at/fhj/swengb/apps/battleship/BattleShipProtocol.scala
@@ -1,11 +1,23 @@
 package at.fhj.swengb.apps.battleship
 
-import at.fhj.swengb.apps.battleship.model.BattleShipGame
+import java.{lang, util}
+
+import scala.collection.JavaConverters._
+import at.fhj.swengb.apps.battleship.model._
 
 object BattleShipProtocol {
 
-  def convert(g : BattleShipGame) : BattleShipProtobuf.BattleShipGame = ???
+  def convert(g : BattleShipGame) : BattleShipProtobuf.BattleShipGame = BattleShipProtobuf.BattleShipGame.newBuilder().build()
 
-  def convert(g : BattleShipProtobuf.BattleShipGame) : BattleShipGame = ???
+  def convert(g : BattleShipProtobuf.BattleShipGame) : BattleShipGame = {
+    val width = g.getBattleField.getWidth
+    val height = g.getBattleField.getHeight
+    val fleet = g.getBattleField.getFleet.getVesselList.asScala.map(convert).toSet
+    val battleField: BattleField = BattleField(width,height,Fleet(fleet))
+    BattleShipGame(battleField, null, null, null)
+  }
 
+  def convert(g: BattleShipProtobuf.Vessel) : Vessel = {
+    Vessel(NonEmptyString("abc"),BattlePos(1,2),Vertical,5)
+  }
 }
