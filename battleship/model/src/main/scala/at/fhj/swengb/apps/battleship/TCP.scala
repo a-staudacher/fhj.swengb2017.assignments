42c3462cde43e3a0c7dea2992c36a287dedde58e
Andreas Staudacher <andreas.staudacher@outlook.de>
2018-01-18 18:25:03 +0100

    created simple TCP Class

---
+++ battleship/model/src/main/scala/at/fhj/swengb/apps/battleship/TCP.scala
@@ -0,0 +1,35 @@
+package at.fhj.swengb.apps.battleship
+
+import java.io.{BufferedReader, InputStreamReader, PrintStream}
+import java.net.{ServerSocket, Socket}
+
+
+
+object TCP {
+  def apply(con:String):TCP = {
+    con match {
+      case "host" => {
+        val sock = new ServerSocket(4444).accept()
+        TCP(sock,1)
+      }
+      case con:String => {
+        val sock: Socket = new Socket(con,4444)
+        TCP(sock,2)
+      }
+    }
+  }
+}
+
+case class TCP(sock:Socket,p:Int) {
+  val is: BufferedReader = new BufferedReader(new InputStreamReader(sock.getInputStream))
+  val os: PrintStream = new PrintStream(sock.getOutputStream())
+
+  def read():String = {
+    is.readLine()
+  }
+
+  def write(msg:String):Unit = {
+    os.println(msg)
+    os.flush()
+  }
+}
