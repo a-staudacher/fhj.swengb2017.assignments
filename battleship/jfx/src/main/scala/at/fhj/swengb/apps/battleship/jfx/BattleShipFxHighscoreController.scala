528c8ab1f4ec97d22ebcb8f8f671242067024908
mhutter07 <martin.hutter@edu.fh-joanneum.at>
2018-01-23 17:01:19 +0100

    ...

--- battleship/jfx/src/main/scala/at/fhj/swengb/apps/battleship/jfx/BattleShipFxHighscoreController.scala
+++ battleship/jfx/src/main/scala/at/fhj/swengb/apps/battleship/jfx/BattleShipFxHighscoreController.scala
@@ -4,6 +4,7 @@ import java.io.{BufferedReader, InputStreamReader, PrintStream}
 import java.net.URL
 import java.nio.file._
 import java.util.ResourceBundle
+import javafx.collections.{FXCollections, ObservableList}
 import javafx.fxml.{FXML, FXMLLoader, Initializable}
 import javafx.scene.layout.GridPane
 import javafx.scene.{Parent, Scene}
@@ -14,32 +15,60 @@ import at.fhj.swengb.apps.battleship.model.{BattleField, BattleShipGame, Fleet, 
 
 import scala.io.Source
 import scala.util.{Failure, Success, Try}
+import javafx.fxml.FXMLLoader
+import javafx.scene.Scene
+import javafx.scene.layout.Pane
+import javafx.stage.Stage
+import javafx.fxml.FXMLLoader
+import javafx.scene.control.{TableView, TextField}
+import javafx.scene.layout.Pane
+import javafx.stage.Stage
+import javafx.scene.control.TableColumn
+
 
 
 class BattleShipFxHighscoreController extends Initializable {
+
+  var num
+  var title = _
+  var score = _
+
   var path: Path = _
   var outstream: BufferedReader = _
+  var tableView : Unit = _
 
   override def initialize(url: URL, rb: ResourceBundle): Unit = {
-  //  val in = Source.fromFile("highscoreFile.csv").getLines().toList
+    val in = Source.fromFile("highscoreFile.csv").getLines().toList
     path = Paths.get("target/highscoreFile.csv")
     outstream = new BufferedReader(new InputStreamReader((Files.newInputStream(path))))
   }
-  def loadGame():Unit = {}
 
-  def bt_newgame(): Unit = {
-    println("test")
-    val scene: Scene = BattleShipFxApp.EditS
-    BattleShipFxApp.loadScene(scene,BattleShipFxApp.rootStage)
-  }
-
-  def bt_highscore():Unit={
-    var line = outstream.readLine()
+  def bt_highscore():Unit= {
+    var line = outstream.readLine().toArray[String]
     while(line != null)
     {
-      println(line)
-      line=outstream.readLine()
+      title = line(0)
+      score = line(1)
+      num = ???
+
+     tableView.getColumn("title")
+
     }
+  }
+  def btnClick1():Unit = {}
+  def btnClick2():Unit = {}
+  def btnClick3():Unit = {}
+  def btnClick4():Unit = {}
+  def btClick5():Unit = {}
+  def btClick6():Unit = {}
+  def btClick7():Unit = {}
+  def btClick8():Unit = {}
+  def btClick9():Unit = {}
+  def btClick10():Unit = {}
+
+  def btnBack():Unit = {
+    val scene: Scene = BattleShipFxApp.MenuS
+    BattleShipFxApp.loadScene(scene, BattleShipFxApp.rootStage)
   }
 
   def bt_credits():Unit = {}
